44dc11aac60e8d352e8282cb4aae6ddf2bb65dc8
Osmar Viana <osmarvianatorres@gmail.com>
2023-05-08 22:30:34 -0300

    "Atualizando portifólio"

---
+++ readme.md
@@ -0,0 +1,59 @@
+# Portifólio Osmar Viana
+
+Portifólio criado para mostrar um pouco do meu conhecimento com programação front-end.
+
+</br>
+
+### Tela Desktop
+
+</br>
+
+<div align='center'>
+<img src="./design/desktop-design.gif">
+</div>
+
+</br>
+
+### Tela Mobile
+
+</br>
+
+<div align='center'>
+<img  src="./design/mobile-design.gif">
+</div>
+
+</br>
+
+## Ferramentas usadas
+
+- HTML
+- CSS
+- JavaScript
+
+</br>
+
+## Links
+
+https://osmarviana.github.io/portifolio-osmarviana
+
+</br>
+
+# Autor
+
+## Osmar Viana
+
+<div align="left"> 
+  <a href="https://www.instagram.com/osmarvianatorres/" target="_blank">
+  <img src="https://img.shields.io/badge/-Instagram-%23D5109A?style=for-the-badge&logo=instagram&logoColor=white" target="_blank" width="150px" height="35px">
+  </a>
+  </br>
+  <a href = "mailto:osmarvianatorres@gmail.com" target="_blank">
+  <img src="https://img.shields.io/badge/-Gmail-%23E4405F?style=for-the-badge&logo=gmail&logoColor=white" target="_blank" width="150px" height="35px">
+  </a>
+  </br>
+  <a href="https://www.linkedin.com/in/osmarvianatorres" target="_blank">
+  <img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank" width="150px" height="35px">
+  </a>
+  </br>
+
+</div>
---
+++ src/js/form-validation.js
@@ -0,0 +1,77 @@
+const form = document.getElementById("form");
+const userName = document.getElementById("name");
+const email = document.getElementById("email");
+const tel = document.getElementById("tel");
+const message = document.getElementById("message");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkForm();
+});
+
+function checkForm() {
+  const nameValue = userName.value;
+  const emailValue = email.value;
+  const telValue = tel.value;
+  const messageValue = message.value;
+
+  if (nameValue.length < 3) {
+    setErrorFor(userName, "O nome de usuário é obrigatório.");
+  } else {
+    setSuccessFor(userName);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "O email é obrigatório.");
+  } else if (!checkEmail(emailValue)) {
+    setErrorFor(email, "Por favor, insira um email válido.");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (telValue === "") {
+    setErrorFor(tel, "O número de telefone é obrigatório.");
+  } else if (telValue.length < 11) {
+    setErrorFor(tel, "Informe um número de telefone válido.");
+  } else {
+    setSuccessFor(tel);
+  }
+
+  if (messageValue === "") {
+    setErrorFor(message, "Você precisa digitar sua mensagem.");
+  } else {
+    setSuccessFor(message);
+  }
+
+  const formControls = form.querySelectorAll(".form-control");
+
+  const formIsValid = [...formControls].every((formControl) => {
+    return formControl.className === "form-control success";
+  });
+
+  if (formIsValid) {
+    alert("Formulário enviado com sucesso!");
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  small.innerText = message;
+
+  formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  formControl.className = "form-control success";
+}
+
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
